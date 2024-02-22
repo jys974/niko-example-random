f9a565c2848c76345d7c6a0885d25345d5bc0d6b
unknown <1026815959@qq.com>
2024-02-22 23:52:11 +0800

    add repository url

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-#!/usr/bin/env node
+// 导入web3库
+const Web3 = require('web3');
 
-const moment = require('moment');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
+// 连接到以太坊节点的地址
+const ethereumUri = 'YOUR_ETHEREUM_NODE_URI';
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ tasks: [] }).write();
+// 创建一个新的web3实例
+const web3 = new Web3(new Web3.providers.HttpProvider(ethereumUri));
 
-function startTask(name) {
-    const task = {
-        name,
-        startTime: new Date(),
-        endTime: null,
-        duration: null
-    };
-    db.get('tasks').push(task).write();
-    console.log(`Task "${name}" started at ${moment(task.startTime).format('YYYY-MM-DD HH:mm:ss')}`);
-}
+// 获取以太坊的区块高度
+web3.eth.getBlockNumber().then(blockNumber => {
+    console.log('当前区块高度：', blockNumber);
+}).catch(error => {
+    console.error('获取区块高度时出错：', error);
+});
 
-function endTask(name) {
-    const task = db.get('tasks').find({ name, endTime: null }).value();
-    if (task) {
-        task.endTime = new Date();
-        task.duration = moment(task.endTime).diff(moment(task.startTime), 'seconds');
-        db.get('tasks').find({ name }).assign(task).write();
-        console.log(`Task "${name}" ended at ${moment(task.endTime).format('YYYY-MM-DD HH:mm:ss')}`);
-        console.log(`Total duration: ${task.duration} seconds`);
-    } else {
-        console.log(`No active task named "${name}" found.`);
-    }
-}
-
-// Sample usage
-startTask('Sample Task');
-setTimeout(() => endTask('Sample Task'), 5000); // Ends the task after 5 seconds
+// 获取账户余额
+const address = 'YOUR_ETH_ADDRESS';
+web3.eth.getBalance(address).then(balance => {
+    console.log('账户余额：', web3.utils.fromWei(balance, 'ether'), 'ETH');
+}).catch(error => {
+    console.error('获取账户余额时出错：', error);
+});
